db72f35df1cfe15197682bdf041a1e5b471fa27e
Pushkar Singh <168922901+hackerpushkar@users.noreply.github.com>
2026-09-18 12:26:26 +0530

    fixing multiple issues

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,7 @@
+from __future__ import annotations
 import html
 import re
-from typing import Dict, Tuple, Optional, List
+from typing import Dict, Tuple, Optional, List, Any
 from telegram import User, InlineKeyboardButton, InlineKeyboardMarkup
 
 
